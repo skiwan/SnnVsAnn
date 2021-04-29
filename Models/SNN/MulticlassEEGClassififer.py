f6c8b6e566593ed6655bc414619b90946599cf2d
skiwan <merlinsewina@web.de>
2021-04-29 15:31:45 +0200

    Changes Forward output to class label

--- Models/SNN/MulticlassEEGClassififer.py
+++ Models/SNN/MulticlassEEGClassififer.py
@@ -8,12 +8,20 @@ class MulticlassEEGClassififer(torch.nn.Module):
     super().__init__()
     self.binary_classifiers = [BinaryEEGClassififer(iz_params[i]) for i in range(4)]
 
-  def forward(self, x, state):
+  def forward(self, xs, state):
     c1_state, c2_state, c3_state, c4_state  = state
+    spikes = [[],[],[],[]]
 
-    c1_x, c1_n_state = self.binary_classifiers[0](x[0], c1_state)
-    c2_x, c2_n_state = self.binary_classifiers[1](x[1], c2_state)
-    c3_x, c3_n_state = self.binary_classifiers[2](x[2], c3_state)
-    c4_x, c4_n_state = self.binary_classifiers[3](x[3], c4_state)
+    for x in xs: 
+        c1_x, c1_state = self.binary_classifiers[0](x[0], c1_state)
+        c2_x, c2_state = self.binary_classifiers[1](x[1], c2_state)
+        c3_x, c3_state = self.binary_classifiers[2](x[2], c3_state)
+        c4_x, c4_state = self.binary_classifiers[3](x[3], c4_state)
+        spikes[0].append(c1_x)
+        spikes[1].append(c2_x)
+        spikes[2].append(c3_x)
+        spikes[3].append(c4_x)
 
-    return torch.stack([c1_x, c2_x, c3_x, c4_x]), (c1_n_state, c2_n_state, c3_n_state, c4_n_state)
+    spikes_c = [sum(s) for s in spikes]
+
+    return spikes_c.index(max(spikes_c))
