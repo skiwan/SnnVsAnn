8c161add85dab944c3df43311c2da6feb527096d
skiwan <merlinsewia@web.de>
2021-05-06 15:46:30 +0200

    Initial cleaned trainin method for binary classifier

--- test_train_binary_class.py
+++ test_train_binary_class.py
@@ -29,30 +29,20 @@ def train_spike(model, data, labels, optimizer, epochs=200):
 
 if __name__ == '__main__':
   initial_params = IzhikevichParameters(0.02,0.2,-65,2)
-  initial_state=IzhikevichState(v=torch.tensor(-70.0, requires_grad=True), u=torch.tensor(-70) * initial_params.b)
+  initial_state=IzhikevichState(v=torch.tensor(-65.0, requires_grad=True), u=torch.tensor(-65) * initial_params.b)
   behaviour = IzhikevichSpikingBehaviour(initial_params, initial_state)
 
   bec = BinaryEEGClassifier(behaviour)
-  data = torch.ones(32,100,4)*8
+  data = torch.ones(32,100,4)*105
   pattern = [0.0, 0.0, 0.0, 0.0, 1.0] * 2
   labels = torch.as_tensor(pattern * 10)
   labels = torch.reshape(labels,(100,1))
 
   bec.input_weights.weight = torch.nn.Parameter(torch.ones_like(bec.input_weights.weight))
-  bec.output_weights.weight = torch.nn.Parameter(torch.ones_like(bec.output_weights.weight)*15)
-
-  #outs = []
-  #states = []
-  #state = (None,None)
-  #for x in data[0]:
-  #  out, state = bec(x, state)
-  #  outs.append(out)
-  #  states.append(state)
-
-
+  bec.output_weights.weight = torch.nn.Parameter(torch.ones_like(bec.output_weights.weight)*210)
 
   optimizer = torch.optim.Adam(bec.parameters(), lr=0.1)
   torch.autograd.set_detect_anomaly(True)
 
-  m1_losses = train_spike(bec, data, labels, optimizer, epochs=5)
+  m1_losses = train_spike(bec, data, labels, optimizer, epochs=2)
   print(m1_losses)
