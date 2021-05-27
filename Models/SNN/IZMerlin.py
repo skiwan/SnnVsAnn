0defa2d3a82495906f44964fd7f62eff57d7ed6d
skiwan <merlinsewia@web.de>
2021-05-27 17:26:17 +0200

    redefines custom iz to an snn module and overrides forward pass without threshold function, still lacks backward

--- Models/SNN/IZMerlin.py
+++ Models/SNN/IZMerlin.py
@@ -1,4 +1,4 @@
-from typing import Tuple, NamedTuple
+from typing import Tuple, NamedTuple, Optional, Any
 
 import torch
 from norse.torch import heaviside
@@ -19,30 +19,39 @@ class CustomIzhikevichSpikingBehaviour(NamedTuple):
 
 
 
-class CustomIzhikevichCell(SNNCell):
+class CustomIzhikevichCell(torch.nn.Module):
 
     def __init__(self, spiking_method: CustomIzhikevichSpikingBehaviour, **kwargs):
-        super().__init__(
-            custom_izhikevich_step, self.initial_state, spiking_method.p, **kwargs
-        )
+        super().__init__()
+        self.activation = custom_izhikevich_step
+        self.state_fallback = self.initial_state
+        self.p = spiking_method.p
+        self.dt = 0.001
         self.spiking_method = spiking_method
+
+    def forward(self, input_tensor: torch.Tensor, state: Optional[Any] = None):
+        state = state if state is not None else self.state_fallback(input_tensor)
+        return self.activation(input_tensor, state, self.p, self.dt)
 
     def initial_state(self, input_tensor: torch.Tensor) -> CustomIzhikevichState:
         state = self.spiking_method.s
         state.v.requires_grad = True
         return state
 
+    def backward(self):
+        pass
+
+def partial_hidden(iz_state: CustomIzhikevichState, p: IzhikevichParameters, dt: float): # autograd would solve this
+    s = iz_state
+    return 1 + p.tau_inv * dt * (2 * s.v * p.sq + p.mn)
 
 
-# TODO Replace dummy implementation
-# Will be partial of state given previous step * previous eligibility trace + partial hidden state over weight
-# Only partial of hidden state over weight for t=1
-def compute_eligibility_trace(iz_state: CustomIzhikevichState):
+def compute_eligibility_trace(iz_state: CustomIzhikevichState, p: IzhikevichParameters, dt: float):
     if iz_state.e.numel() == 0:
-        new_e = torch.ones(iz_state.u.size()[0]) # TODO replace with partial hidden state over weight function of same dimension as number of neurons
+        new_e = partial_hidden(iz_state, p, dt)
     else:
         old_e = iz_state.e[:, -1:] # last eligibility trace
-        new_e = torch.add(old_e, torch.ones(iz_state.u.size()[0])) # Todo replace with correct eligibility trace function
+        new_e = torch.add(old_e, partial_hidden(iz_state, p, dt))
     return new_e
 
 def custom_izhikevich_step(
@@ -60,19 +69,10 @@ def custom_izhikevich_step(
     e_ = compute_eligibility_trace(s)
     e_ = torch.column_stack((s.e, e_))
 
-    z_ = eprop_fn(v_ - p.v_th, torch.as_tensor(p.alpha), e_)
+    z_ = heaviside(v_ - p.v_th)
     v_ = (1 - z_) * v_ + z_ * p.c
     u_ = (1 - z_) * u_ + z_ * (u_ + p.d)
     return z_, CustomIzhikevichState(v_, u_, e_)
-
-# TODO v2 Needs to compute eligibility traces here and make them accesable without changing IZ state
-class CustomEProp(torch.autograd.Function):
-    @staticmethod
-    @torch.jit.ignore
-    def forward(ctx, input_tensor: torch.Tensor, alpha: float, eligibility_trace: torch.Tensor) -> torch.Tensor:
-        ctx.save_for_backward(input_tensor, eligibility_trace) # save trace for backward
-        ctx.alpha = alpha
-        return heaviside(input_tensor)
 
     @staticmethod
     @torch.jit.ignore
@@ -85,8 +85,3 @@ class CustomEProp(torch.autograd.Function):
         )  #pseudo derivative
         grad = torch.mul(grad, e_traces)
         return grad, None #TODO Q: Why return 2 vals here?
-
-
-@torch.jit.ignore
-def eprop_fn(x: torch.Tensor, alpha: float = 100.0) -> torch.Tensor:
-    return CustomEProp.apply(x, alpha)
