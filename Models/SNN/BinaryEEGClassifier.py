9deef8b571c9440b7eead2b4c89d02d1322ed497
skiwan <merlinsewia@web.de>
2021-06-03 18:13:45 +0200

    Adds gaining factor linear layers to help network spike

--- Models/SNN/BinaryEEGClassifier.py
+++ Models/SNN/BinaryEEGClassifier.py
@@ -1,30 +1,29 @@
+from norse.torch import SequentialState, LIFCell
 from norse.torch.module import IzhikevichCell
 import torch
 
 class BinaryEEGClassifier(torch.nn.Module):
-  def __init__(self, channels,p):
+  def __init__(self, channels,p, gaining_factor=1):
     super().__init__()
-    self.input_weights = torch.nn.Linear(channels, 2) # 2 is number of IZ Neurons
-    self.input_layer = IzhikevichCell(p)
-    self.output_weights = torch.nn.Linear(2, 1) # 2 is number of iZ neurons above, 1 is target number of output neurons
-    self.output_neuron = IzhikevichCell(p)
+    gaining_layer_1 = torch.nn.Linear(channels,channels)
+    gaining_layer_2 = torch.nn.Linear(2,2)
+    gaining_layer_1.weight = torch.nn.Parameter(torch.ones_like(gaining_layer_1.weight)*gaining_factor)
+    gaining_layer_1.requires_grad = False
+    gaining_layer_2.weight = torch.nn.Parameter(torch.ones_like(gaining_layer_2.weight) * gaining_factor)
+    gaining_layer_2.requires_grad = False
 
-  def forward(self, xs, state=(None, None)): # xs has dim timesteps , channels
-    input_state, output_state = state
-    # multiply whole input by weight matrix to scale input
-    xs = self.input_weights(xs) # transform the input from timesteps x channels into timesteps x neuron amount (100,4) to (100,2)
-    # for each time step compute spike train of input layer
-    input_spikes = []
-    for x in xs: # run all timesteps and record spikes for each neuron (list of timesteps x spikes_per_neuron)
-      spike, input_state = self.input_layer(x, input_state)
-      input_spikes.append(spike)
-    input_spikes = torch.stack(input_spikes) # make a tensor out of the list
-    # multiply whole spike train by weight matrix to scale to new input current
-    xs = self.output_weights(input_spikes) # multiply spikes with output weights to get new input current train for output neuron timesteps x 1
-    # for each time step compute spike train for output neuron
-    output_spikes = []
-    for x in xs:
-      spike, output_state = self.output_neuron(x, output_state)
-      output_spikes.append(spike)
-    output_spikes = torch.stack(output_spikes) # timesteps x 1 output spikes
-    return output_spikes, (input_state, output_state)
+    self.model = SequentialState(
+    gaining_layer_1,
+    torch.nn.Linear(channels,2),
+    IzhikevichCell(spiking_method=p),
+    gaining_layer_2,
+    torch.nn.Linear(2,1),
+    IzhikevichCell(spiking_method=p)
+    )
+
+  def forward(self, xs, state=None):
+    spikes = []
+    for ts in xs:
+       s, state = self.model.forward(ts, state)
+       spikes.append(s)
+    return torch.stack(spikes)
