08a670cc35c8e19a827f8873e1a9220a076fc755
skiwan <merlinsewia@web.de>
2021-06-17 15:12:03 +0200

    Implements Multiclass classifier for SNN

--- Models/SNN/FourClassSNNLayer.py
+++ Models/SNN/FourClassSNNLayer.py
@@ -8,18 +8,16 @@ class FourClassSNNLayer(torch.nn.Module):
     self.class_one_classifier = BinaryEEGClassifier(channels=all_channel_info[0], p=all_class_iz_params[0], gaining_factor=gaining_factor)
     self.class_two_classifier = BinaryEEGClassifier(channels=all_channel_info[1], p=all_class_iz_params[1], gaining_factor=gaining_factor)
     self.class_three_classifier = BinaryEEGClassifier(channels=all_channel_info[2], p=all_class_iz_params[2], gaining_factor=gaining_factor)
-    self.class_four_classifier = BinaryEEGClassifier(achannels=all_channel_info[3], p=all_class_iz_params[3], gaining_factor=gaining_factor)
+    self.class_four_classifier = BinaryEEGClassifier(channels=all_channel_info[3], p=all_class_iz_params[3], gaining_factor=gaining_factor)
 
 
   # input to the forward function should be a touple of 4 data samples and a touple of 4 states
   # a data sample should be timesteps x channels
-  def forward(self, xs, state):
-    c1_state, c2_state, c3_state, c4_state = state
+  def forward(self, xs):
     c1_in, c2_in, c3_in, c4_in = xs
-
-    spikes_one = torch.sum(self.class_one_classifier.forward(c1_in, c1_state))
-    spikes_two = torch.sum(self.class_two_classifier.forward(c2_in, c2_state))
-    spikes_three = torch.sum(self.class_three_classifier.forward(c3_in, c3_state))
-    spikes_four = torch.sum(self.class_four_classifier.forward(c4_in, c4_state))
+    spikes_one = torch.sum(self.class_one_classifier.forward(c1_in))
+    spikes_two = torch.sum(self.class_two_classifier.forward(c2_in))
+    spikes_three = torch.sum(self.class_three_classifier.forward(c3_in))
+    spikes_four = torch.sum(self.class_four_classifier.forward(c4_in))
 
     return torch.stack([spikes_one,spikes_two,spikes_three,spikes_four])
--- Models/SNN/MulticlassEEGClassififer.py
+++ Models/SNN/MulticlassEEGClassififer.py
@@ -7,13 +7,13 @@ class MulticlassEEGClassififer(torch.nn.Module):
   def __init__(self, all_channel_info, all_class_iz_params, gaining_factor):
     super().__init__()
     self.four_class_snn_layer = FourClassSNNLayer(all_channel_info=all_channel_info, all_class_iz_params=all_class_iz_params, gaining_factor=gaining_factor)
-    self.softmax = torch.nn.Softmax(dim=1) #TODO Check if Dim is necessary here
+    self.softmax = torch.nn.LogSoftmax(dim=0)
 
     self.model = SequentialState(
         self.four_class_snn_layer,
         self.softmax
     )
 
-  def forward(self, xs, state):
-      return self.model.forward(xs, state)
+  def forward(self, xs):
+      return self.model.forward(xs)
 
