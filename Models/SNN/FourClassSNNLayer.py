a8030a1acb6877fd2c0e06205493fc7e1a02c07b
skiwan <merlinsewia@web.de>
2021-06-17 11:33:23 +0200

    Adds input description as well as fixes class name

--- Models/SNN/FourClassSNNLayer.py
+++ Models/SNN/FourClassSNNLayer.py
@@ -2,7 +2,7 @@ from .BinaryEEGClassifier import BinaryEEGClassifier
 import torch
 
 
-class MulticlassEEGClassififer(torch.nn.Module):
+class FourClassSNNLayer(torch.nn.Module):
   def __init__(self, all_channel_info, all_class_iz_params, gaining_factor):
     super().__init__()
     self.class_one_classifier = BinaryEEGClassifier(channels=all_channel_info[0], p=all_class_iz_params[0], gaining_factor=gaining_factor)
@@ -10,6 +10,9 @@ class MulticlassEEGClassififer(torch.nn.Module):
     self.class_three_classifier = BinaryEEGClassifier(channels=all_channel_info[2], p=all_class_iz_params[2], gaining_factor=gaining_factor)
     self.class_four_classifier = BinaryEEGClassifier(achannels=all_channel_info[3], p=all_class_iz_params[3], gaining_factor=gaining_factor)
 
+
+  # input to the forward function should be a touple of 4 data samples and a touple of 4 states
+  # a data sample should be timesteps x channels
   def forward(self, xs, state):
     c1_state, c2_state, c3_state, c4_state = state
     c1_in, c2_in, c3_in, c4_in = xs
