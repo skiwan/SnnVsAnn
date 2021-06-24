7225b817d7d67f069403f0f7d965a801bdf49299
skiwan <merlinsewia@web.de>
2021-06-24 16:32:17 +0200

    Implements base scnn network

--- Models/ANN/BaseSCNN.py
+++ Models/ANN/BaseSCNN.py
@@ -3,18 +3,19 @@ import torch
 
 
 class BaseSCNN(torch.nn.Module):
-  def __init__(self, channels, base_filters, classes, image_width=200, image_height=44, padding=0, stride=0):
+  def __init__(self, channels, base_filters, classes, image_width=200, image_height=44, padding=0, stride=1):
     super().__init__()
     self.model = SequentialState(
         torch.nn.Conv2d(channels, base_filters, kernel_size=(image_height, 1), padding=padding, stride=stride),
-        torch.nn.BatchNorm2d(200),
+        torch.nn.BatchNorm2d(base_filters),
         torch.nn.ReLU(),
-        torch.nn.Conv2d(channels, base_filters*2, kernel_size=(1, 10), padding=padding, stride=stride),
-        torch.nn.BatchNorm2d(200),
+        torch.nn.Conv2d(base_filters, base_filters*2, kernel_size=(1, 10), padding=padding, stride=stride, dilation=(1,20)),
+        torch.nn.BatchNorm2d(base_filters*2),
         torch.nn.ReLU(),
         torch.nn.Flatten(),
-        torch.nn.Linear(1, classes),
-        torch.nn.Softmax()
+        torch.nn.Linear(base_filters*40, base_filters*8),
+        torch.nn.Linear(base_filters*8, classes),
+        #torch.nn.Softmax(dim=1) Excluded to be able to better use this in advanced models
     )
 
   def forward(self, xs):
--- fiddle.py
+++ fiddle.py
@@ -1,6 +1,6 @@
 from Models.ANN.BaseSCNN import BaseSCNN
 from torchsummary import summary
 
-model = BaseSCNN(channels=3, base_filters=8, classes=4, image_width=200, image_height=44, padding=0, stride=0).to('cpu')
-print(summary(model, (44,200,3)))
+model = BaseSCNN(channels=3, base_filters=8, classes=2, image_width=200, image_height=44).to('cpu')
+print(summary(model, (3,44,200), device='cpu'))
 print('Done')
