6c60864d7c945db966597e22218e179ebc2aa053
skiwan <merlinsewia@web.de>
2021-07-06 13:40:20 +0200

    Implements test training of multi class with base scnn (still needs to be tested with eval set)

--- test_train_multi_class_scnn.py
+++ test_train_multi_class_scnn.py
@@ -1,23 +1,23 @@
-from Models.SNN.MulticlassEEGClassififer import MulticlassEEGClassififer
-
+from Models.ANN.BaseSCNN import BaseSCNN
 import torch
-from norse.torch.functional.izhikevich import IzhikevichSpikingBehaviour, IzhikevichState, IzhikevichParameters
-
+import numpy as np
+import os
 import time
 
-def train_spike(model, data, labels, optimizer, epochs=200):
+def train(model, data, labels, optimizer, epochs=200):
     losses = []
     start = time.time()
+    loss = torch.nn.NLLLoss()
+    log = torch.nn.LogSoftmax(dim=1)
     for i in range(epochs): # repeat this epoch amount of times
         print(f'epoch {i} out of {epochs}')
         epoch_losses = []
-        for i in range(len(data)): # take a single sample of shape classes x timesteps x channels (4,100,C)
-            xs = data[i]
+        for i in range(len(data)):
+            xs = data[i].unsqueeze(0)
             label = labels[i]
             out, _ = model(xs)
-            activity_in_correct_class_loss = torch.tensor(0.0, requires_grad=True) if out[label] > 0 else torch.tensor(7.5, requires_grad=True)
-            correct_class_loss = torch.tensor(0.0, requires_grad=True) if torch.argmax(out) == label else torch.tensor(2.0, requires_grad=True)
-            loss_v = activity_in_correct_class_loss + correct_class_loss
+            out = log(out)
+            loss_v = loss(out, torch.tensor(label-1).unsqueeze(0))
             epoch_losses.append(loss_v)
         epoch_loss = torch.stack(epoch_losses).mean(0)
         optimizer.zero_grad()
@@ -31,66 +31,41 @@ def train_spike(model, data, labels, optimizer, epochs=200):
 
 
 if __name__ == '__main__':
-    gaining_factor = 70
-    timesteps = 100
-    all_channel_info = [4,3,5,4]
-    samples = 40
+    current_wd = os.getcwd()
 
-    initial_params = IzhikevichParameters(a=0.02, b=0.2, c=-65, d=2)
-    initial_state = IzhikevichState(v=torch.tensor(-65.0, requires_grad=True), u=torch.tensor(-65) * initial_params.b)
-    behaviour = IzhikevichSpikingBehaviour(initial_params, initial_state)
+    model = BaseSCNN(channels=25, base_filters=8, classes=4, image_height=44).to('cpu')
+    model.float()
 
-    all_iz_params = [behaviour, behaviour, behaviour, behaviour]
+    dataset = np.load(os.path.join(current_wd, os.path.join('Raw_Preprocessed_CWT', 'BCI4_2a_A01T_car_7_30.npy')))
+    dataset = dataset[:,:,:,:200]
+    dataset = torch.from_numpy(dataset).float()
+    #ev_set = np.load()
 
-    mceeg = MulticlassEEGClassififer(all_channel_info=all_channel_info, all_class_iz_params=all_iz_params, gaining_factor=gaining_factor).cuda()
+    with open(os.path.join(current_wd, os.path.join('Raw_Preprocessed_CWT', 'BCI4_2a_A01T_car_labels.txt'))) as labelfile:
+        lines = labelfile.readlines()
+        data_labels = [int(l.replace('\n','')) for l in lines]
+    #ev_labels =
 
+    print(dataset.shape)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
-    # create 50 samples each with 100 60 30 and 0 activity for class 4 to 1
-    class_1_freq = [0.] * timesteps
-
-    class_2_freq = [1.,0.,0.,1.,0.] * timesteps
-    class_2_freq = class_2_freq[:timesteps]
-
-    class_3_freq = [1.,1.,0.] * timesteps
-    class_3_freq = class_3_freq[:timesteps]
-
-    class_4_freq = [1.] * timesteps
-
-    class_freq = {
-        'class_1_freq' : class_1_freq,
-        'class_2_freq' : class_2_freq,
-        'class_3_freq' : class_3_freq,
-        'class_4_freq' : class_4_freq
-    }
-
-    class_freq_inputs = {}
-    for c in range(4):
-        class_freq_inputs[f'{c}'] = []
-
-    # samples * classes * timesteps * channels
-    single_channel_inputs = []
-    for i in range(len(all_channel_info)):
-        channel_amount = all_channel_info[i]
-        for c in range(4):
-            single_data = [class_freq[f'class_{c+1}_freq'] for x in range(channel_amount)]
-            single_data = torch.tensor(single_data)
-            single_data = torch.swapaxes(single_data, 0, 1)
-            class_freq_inputs[f'{i}'].append(single_data.cuda())
-
-    inputs = []
-    for i in range(4):
-        single = []
-        for x in range(4):
-            single.append(class_freq_inputs[f'{x}'][i])
-        inputs.append(single)
-
-    data = [inputs[i] for i in range(4)] * (samples//4)
-    labels = [torch.tensor([i]).cuda() for i in range(4)] * (samples//4)
-
-    optimizer = torch.optim.Adam(mceeg.parameters(), lr=0.01)
-    torch.autograd.set_detect_anomaly(True)
-
-    m1_losses = train_spike(mceeg, data, labels, optimizer, epochs=500)
+    m1_losses = train(model, dataset, data_labels, optimizer, epochs=50)
     print(m1_losses)
     print(min(m1_losses))
-    torch.save(mceeg, 'multiclassmodelfakedata.pth')
+
+    softmax = torch.nn.Softmax(dim=1)
+    predictions = []
+    for x in dataset:
+        pre, _ = model(x.unsqueeze(0))
+        pre = softmax(pre)
+        pre = torch.argmax(pre)
+        predictions.append(pre)
+
+    acc = 0
+    for p in range(len(predictions)):
+        pre = predictions[p]
+        truth = data_labels[p]-1
+        if pre == truth:
+            acc += 1
+    print(acc, acc/len(predictions))
+    print('done')
