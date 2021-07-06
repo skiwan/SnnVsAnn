416b8af530964a8f7ac0924f733fbe806cadc351
skiwan <merlinsewia@web.de>
2021-07-06 13:39:41 +0200

    Addapts fiddle to ignore image widht

--- fiddle.py
+++ fiddle.py
@@ -1,6 +1,6 @@
 from Models.ANN.BaseSCNN import BaseSCNN
 from torchsummary import summary
 
-model = BaseSCNN(channels=3, base_filters=8, classes=2, image_width=200, image_height=44).to('cpu')
-print(summary(model, (3,44,200), device='cpu'))
+model = BaseSCNN(channels=25, base_filters=8, classes=2, image_height=44).to('cpu')
+print(summary(model, (25,44,200), device='cpu'))
 print('Done')
