eca4e86f9cc7f6ad4ea34bdcd2ab72d696317c95
skiwan <merlinsewia@web.de>
2021-07-10 18:06:44 +0200

    Add Validation Loss

--- test_train_multi_class_scnn.py
+++ test_train_multi_class_scnn.py
@@ -3,18 +3,47 @@ import torch
 import numpy as np
 import os
 import time
+from random import randint
 
-def train(model, data, labels, optimizer, epochs=200):
+def random_split(data, labels, splitnr):
+    train_d = []
+    train_l = []
+
+    val_d = []
+    val_l = []
+
+    d = data.detach().clone()
+    l = list(labels)
+    r = list(range(len(l)))
+    for i in range(splitnr):
+        pick = randint(0,len(r)-1)
+        pick = r[pick]
+        train_d.append(d[pick])
+        train_l.append(l[pick])
+        r.remove(pick)
+
+    for n in r:
+        val_d.append(d[n])
+        val_l.append(l[n])
+
+    return train_d, train_l, val_d, val_l
+
+
+def train(model, train_data, train_labels, val_data, val_labels, optimizer, epochs=200):
     losses = []
+    val_losses = []
     start = time.time()
     loss = torch.nn.NLLLoss()
     log = torch.nn.LogSoftmax(dim=1)
+
+    last_min_val_loss = None
+
     for i in range(epochs): # repeat this epoch amount of times
         print(f'epoch {i} out of {epochs}')
         epoch_losses = []
-        for i in range(len(data)):
-            xs = data[i].unsqueeze(0)
-            label = labels[i]
+        for i in range(len(train_data)):
+            xs = train_data[i].unsqueeze(0)
+            label = train_labels[i]
             out, _ = model(xs)
             out = log(out)
             loss_v = loss(out, torch.tensor(label-1).unsqueeze(0))
@@ -24,9 +53,25 @@ def train(model, data, labels, optimizer, epochs=200):
         epoch_loss.backward()
         optimizer.step()
         losses.append(epoch_loss.detach())
-        print(f'min epoch loss {min(losses)} min average epoch loss {min(epoch_losses)}')
-        print(f'max epoch loss {max(losses)} max average epoch loss {max(epoch_losses)}')
+
+        val_epoch_losses = []
+        for i in range(len(val_data)):
+            xs = val_data[i].unsqueeze(0)
+            label = val_labels[i]
+            out, _ = model(xs)
+            out = log(out)
+            loss_v = loss(out, torch.tensor(label-1).unsqueeze(0))
+            val_epoch_losses.append(loss_v)
+        val_epoch_loss = torch.stack(val_epoch_losses).mean(0)
+        val_losses.append(val_epoch_loss)
+
+        print(f'min average epoch loss {min(losses)} min current epoch loss {min(epoch_losses)}')
+        print(f'min average val epoch loss {min(val_losses)} min current val epoch loss {min(val_epoch_losses)}')
+        print(f'max average epoch loss {max(losses)} max current epoch loss {max(epoch_losses)}')
+        print(f'max average val epoch loss {max(val_losses)} max val current epoch loss {max(val_epoch_losses)}')
     print(f"Training for {epochs} took a total of {time.time()-start} seconds")
+
+    print(f'Minimum overall average epoch val loss was {min(val_epoch_losses)} at epoch {val_epoch_losses.index(min(val_epoch_losses))}')
     return losses
 
 
@@ -49,9 +94,11 @@ if __name__ == '__main__':
     ev_labels = np.load(os.path.join(os.path.join(current_wd, os.path.join('Raw_Preprocessed', 'A01E_labels.npy'))))
 
     print(dataset.shape)
+
+    train_data, train_labels, test_data, test_labels = random_split(dataset, data_labels, 240)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
-    m1_losses = train(model, dataset, data_labels, optimizer, epochs=50)
+    m1_losses = train(model, train_data,train_labels, test_data, test_labels, optimizer, epochs=50)
     print(m1_losses)
     print(min(m1_losses))
 
