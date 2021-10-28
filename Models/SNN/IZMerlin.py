1899c3f17fd95808bf0a394ca8583051a676d70c
skiwan <merlinsewia@web.de>
2021-10-28 16:12:42 +0200

    Adds remains if old eprop implementaiton

--- Models/SNN/IZMerlin.py
+++ Models/SNN/IZMerlin.py
@@ -1,5 +1,5 @@
 from typing import Tuple, NamedTuple, Optional, Any
-
+import numpy as np
 import torch
 from norse.torch import heaviside
 from norse.torch.module.snn import SNNCell
@@ -38,37 +38,80 @@ class CustomIzhikevichCell(torch.nn.Module):
         state.v.requires_grad = True
         return state
 
-    def backward(self, state: CustomIzhikevichState, learning_signals):
+    def pseudo_derivative(self, v , p: IzhikevichParameters):
+        v_th = torch.tensor([p.v_th for i in range(v.size()[0])])
+        s = (1 - np.abs((v.detach() - v_th) / v_th))
+        t = np.maximum(np.zeros_like(v.detach().numpy()), s)
+        return 1 / v_th * 0.3 * t
+
+    def compute_eligibility_trace(self, v, p: IzhikevichParameters, eligibility_vector):
+        pseudo = self.pseudo_derivative(v, p)
+        return torch.mul(pseudo, eligibility_vector)
+
+    def backward(self, state: CustomIzhikevichState, voltages, learning_signals, p: IzhikevichParameters):
         random_weight = 0.5
         print(state.e.size())
         if state.e.size()[0] != len(learning_signals):
-            print('Length of Learning Signals and eligbility traces is not equal')
+            print('Length of Learning Signals and eligbility vectors is not equal')
+            return None
+        if state.e.size()[0] != len(voltages):
+            print('Length of voltages and eligbility vectors is not equal')
             return None
         grads = []
         states = state.e
-        #TODO Q: Do I take first or last eligibility trace?
+        #TODO Q: Do I take first or last eligibility trace? > first one because of learning signal
         while states.numel() > 0:
-            trace = state.e[-1:, :]
-            ls = learning_signals[-1]
+            e_vec = state.e[:1, :]
+            voltage = voltages[0]
+            ls = learning_signals[0]
+            trace = self.compute_eligibility_trace(voltage, p, e_vec)
             grads.append(ls * trace * random_weight)
-            states = states[:-1, :]
-            learning_signals = learning_signals[:-1]
+            states = states[1:, :]
+            learning_signals = learning_signals[1:]
+            voltages = voltages[1:]
         print('No eligibility traces left')
         self.grad = grads[-1]
+
         return grads
 
-def partial_hidden(iz_state: CustomIzhikevichState, p: IzhikevichParameters, dt: float): # autograd would solve this
-    s = iz_state
-    return 1 + p.tau_inv * dt * (2 * s.v * p.sq + p.mn)
 
+def v_over_v(iz_state: CustomIzhikevichState, p: IzhikevichParameters, dt: float):
+    return 1 + p.tau_inv * dt * (2*iz_state.v * p.sq + p.mn)
 
-def compute_eligibility_trace(iz_state: CustomIzhikevichState, p: IzhikevichParameters, dt: float):
-    if iz_state.e.numel() == 0:
-        new_e = partial_hidden(iz_state, p, dt)
+def v_over_u(p: IzhikevichParameters, dt: float):
+    return - p.tau_inv * dt
+
+def u_over_v(p: IzhikevichParameters, dt: float):
+    return p.tau_inv * dt * p.a * p.b
+
+def u_over_u(p: IzhikevichParameters, dt: float):
+    return 1 + p.tau_inv * dt * p.a * (-1)
+
+def v_over_w(p: IzhikevichParameters, dt: float):
+    return p.tau_inv * dt
+
+def u_over_w():
+    return 0
+
+def compute_eligibility_vector(iz_state: CustomIzhikevichState, p: IzhikevichParameters, dt: float, previous_eligibility_vector=None):
+    sec = torch.tensor([v_over_w(p, dt), u_over_w()])
+    a = sec.size()[0]
+    x = iz_state.v.size()[0]
+    sec = torch.cat(x*[sec])
+    sec = torch.reshape(sec, (x, a))
+    if previous_eligibility_vector is None:
+        return sec
     else:
-        old_e = iz_state.e[:, -1:] # last eligibility trace
-        new_e = torch.mul(old_e, partial_hidden(iz_state, p, dt))
-    return new_e
+        vov = v_over_v(iz_state, p, dt)
+        vou = v_over_u(p, dt)
+        uov = u_over_v(p, dt)
+        uou = u_over_u(p, dt)
+        jacobian = torch.tensor([[vov, vou],[uov, uou]])
+        fir = (jacobian * previous_eligibility_vector + sec)
+        return fir
+
+
+
 
 # TODO eligibility trace needs right dimensions for time and multi neurons
 def custom_izhikevich_step(
@@ -82,17 +125,23 @@ def custom_izhikevich_step(
     )
     u_ = s.u + p.tau_inv * dt * p.a * (p.b * s.v - s.u)
 
-    s_ =  CustomIzhikevichState(v_, u_, s.e)
+    z_ = heaviside(v_ - p.v_th)
+    v_ = (1 - z_) * v_ + z_ * p.c
+    u_ = (1 - z_) * u_ + z_ * (u_ + p.d)
 
     # Todo Q:should this happen before or after membrane reset?
-    e_ = compute_eligibility_trace(s_, p, dt)
+    #  > given OSTL paper this should happen after Cell computation
+    s_ = CustomIzhikevichState(v_, u_, s.e)
+    if s_.e.numel() > 0:
+        last_e = s_.e[:, -1:]  # last eligibility trace
+    else:
+        last_e = None
+
+    e_ = compute_eligibility_vector(s_, p, dt, last_e)
     if s_.e.numel() > 0:
         e_ = torch.column_stack((s_.e, e_))
     else:
         e_ = torch.unsqueeze(e_,0)
 
-    z_ = heaviside(v_ - p.v_th)
-    v_ = (1 - z_) * v_ + z_ * p.c
-    u_ = (1 - z_) * u_ + z_ * (u_ + p.d)
     return z_, CustomIzhikevichState(v_, u_, e_)
 
--- test_custom_iz.py
+++ test_custom_iz.py
@@ -8,10 +8,10 @@ CIB = CustomIzhikevichSpikingBehaviour(p=IP, s=CIS)
 CIZ = CustomIzhikevichCell(CIB)
 
 # Single timestep
-out, n_state = CIZ.forward(torch.tensor([1,1]))
-loss = torch.nn.functional.mse_loss(out, torch.tensor([1,1]))
-grads = CIZ.backward(n_state, [loss])
-print(grads)
+#out, n_state = CIZ.forward(torch.tensor([1,1]))
+#loss = torch.nn.functional.mse_loss(out, torch.tensor([1,1]))
+#grads = CIZ.backward(n_state,[n_state.v], [loss], IP)
+#print(grads)
 
 
 
@@ -21,10 +21,12 @@ print(grads)
 n_state = None
 input = torch.tensor([1,1])
 outs = []
+voltages = []
 learning_s = [torch.tensor([1,1]) for x in range(10)]
 for t in range(10):
     out, n_state = CIZ.forward(input, n_state)
+    voltages.append(n_state.v)
     outs.append(out)
 loss = torch.nn.functional.mse_loss(torch.stack(outs), torch.stack(learning_s))
-grads = CIZ.backward(n_state, [loss])
+grads = CIZ.backward(n_state,voltages, [loss], IP)
 print(grads)
