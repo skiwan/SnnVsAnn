c748a263fec82558fd1b5b7b9264f8628d833681
skiwan <merlinsewia@web.de>
2021-11-11 17:08:04 +0100

    Implements basic data splitter

---
+++ Models/data_splitter.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+class DataSplitter():
+    def __init__(self, dataset_path, labels_path, target_path, ratio):
+        self.dataset = np.load(dataset_path)
+        self.labels = np.load(labels_path)
+        self.ratio = ratio
+        self.target_path = target_path
+
+
+    def split(self):
+        idxs = [i for i in range(len(self.labels))]
+        idxs = np.random.permutation(idxs)
+        cutoff = int(len(self.labels) * self.ratio)
+        train_idxs = idxs[:cutoff]
+        validate_idxs = idxs[cutoff:]
+
+        train_data = self.dataset[train_idxs]
+        train_labels =  self.labels[train_idxs]
+
+        validate_data = self.dataset[validate_idxs]
+        validate_labels = self.labels[validate_idxs]
+
+        np.save(train_data, f'{self.target_path}_train_data.npy')
+        np.save(train_labels, f'{self.target_path}_train_labels.npy')
+        np.save(validate_data, f'{self.target_path}_validate_data.npy')
+        np.save(validate_labels, f'{self.target_path}_validate_labels.npy')
