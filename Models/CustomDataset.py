b7000603036a7ebcc0550a96b2dc3f94cf088ce0
skiwan <merlinsewia@web.de>
2021-11-16 14:19:33 +0100

    Fixes variable name

--- Models/CustomDataset.py
+++ Models/CustomDataset.py
@@ -5,7 +5,7 @@ class CustomDataset(Dataset):
 
     def __init__(self, label_file, data_file):
         self.data_labels = np.load(label_file)
-        self.data_ = np.load(data_file)
+        self.data = np.load(data_file)
 
     def __len__(self):
         return len(self.data_labels)
