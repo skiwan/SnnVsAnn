52fb2dd744bfbc9308351dae65a152eff84cc24d
skiwan <merlinsewia@web.de>
2021-11-18 18:56:43 +0100

    Adds utils file for scripts

---
+++ Scripts/Utils.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+
+def create_folder_if_not_exists(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def create_temp_folder(base_folder):
+    temp_path = os.path.join(base_folder, 'temp')
+    create_folder_if_not_exists(temp_path)
+
+def delete_temp_folder(base_folder):
+    temp_path = os.path.join(base_folder, 'temp')
+    if os.path.exists(temp_path):
+        shutil.rmtree(temp_path)
