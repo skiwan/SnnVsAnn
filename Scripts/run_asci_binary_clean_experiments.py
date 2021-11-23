4602f507cd08ee09f6f6b835e45573b34f3fd8cb
skiwan <merlinsewia@web.de>
2021-11-23 19:04:56 +0100

    Adds ASCI Based ANN experiment runs

---
+++ Scripts/run_asci_binary_clean_experiments.py
@@ -0,0 +1,11 @@
+from run_basic_training_binary_asci import main as run_training
+
+
+if __name__ == '__main__':
+    subjects = [['k3b','1'],['k6b','2'],['l1b','3']]
+
+    for s in subjects:
+        experiment_name = f'Binary_ANN_{s[0]}'
+        experiment_description = 'One vs Rest classification of ASCI based motor imagery classifcation on ANN Architecture. ' \
+                                 'Applies Error averaging, CSP, Normalization and then transforms into CWT'
+        run_training(experiment_name, experiment_description, subject_name=s[0], subject_nr=s[1])
