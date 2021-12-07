307e919072ff45522a8f75d59761cbce89fa40ca
skiwan <merlinsewia@web.de>
2021-12-07 15:46:23 +0100

    optimizes paralleilzation of gdf experimetns

--- Scripts/run_basic_training_gdf.py
+++ Scripts/run_basic_training_gdf.py
@@ -9,57 +9,26 @@ from Scripts.multi_class_ann_run import main as multiclass_run
 import shutil
 import os, sys
 import json
+import math
+import threading
 
-def main(experiment_name, experiment_description, train_file_name, eval_file_name, eval_label_file_name,
-         learning_rate, weight_decay, cut_off_front, cut_off_back, dropout,
-         result_collector={}, experiment_number=0 ,device='cuda'):
-    file_directory = os.path.dirname(os.path.abspath(__file__))
-    parent_folder = os.path.dirname(file_directory)
-    base_save_path = os.path.join(file_directory, 'temp/')
-
-    # Create temp folder if not exists
-    create_temp_folder(file_directory)
-
-    experiment_name = experiment_name #'Binary_ANN_A01'
-    experiment_description = experiment_description# 'Something Something'
-    train_file_name = train_file_name# 'A01T.gdf'
-    eval_file_name = eval_file_name# 'A01E.gdf'
-    eval_label_file_name = eval_label_file_name#'A01E_labels.npy'
-
-    base_file_path = os.path.join(parent_folder, 'original_data/Datasets/BCICompetitionIV/Data/BCICIV_2a_gdf/')
-    base_label_path = os.path.join(parent_folder, 'savecopywithlabels/')
-
-    low_pass = 7
-    high_pass = 30
-    raw_train_file_name = os.path.join(base_file_path, train_file_name)
-    raw_eval_file_name = os.path.join(base_file_path, eval_file_name)
-    extract_features = False
-    scale_1 = [7,15,0.5]
-    scale_2 = [16,30,0.5]
-    split_ratio = 0.7
-    splitting_strategy = 'balanced-copy'
-    batch_size = 32
-    shuffle = True
-    workers = 10
-    max_epochs = 100
-    model_channels = 8
-    model_classes = 2
+def run_threaded_model(base_save_path, batch_size, cut_off_back, cut_off_front, device, dropout, eval_file_name,
+                       experiment_description, experiment_name, experiment_number, experiment_setup_info,
+                       extract_features, file_directory, high_pass, learning_rate, low_pass, max_epochs, model_channels,
+                       model_classes, result_collector, scale_1, scale_2, shuffle, split_ratio, splitting_strategy,
+                       train_file_name, weight_decay, workers):
+    result_collector[experiment_number] = {}
     model_dropout = dropout
     model_learning_rate = learning_rate
     model_weight_decay = weight_decay
     data_cut_front = cut_off_front
     data_cut_back = cut_off_back
+    experiment_name = f'{experiment_name}_learnrate{learning_rate}_weightdec{weight_decay}_cutoff{cut_off_front}_{cut_off_back}_drop{dropout}'
     save_model = True
-
-
-    # Potential HyperParams
-    # Learninr Rate, Weight Decay, Data Cutoff, OPtimisier, Dropout
-
     destination_path = os.path.join(file_directory, 'Experiments')
     destination_path = os.path.join(destination_path, f'{experiment_name}')
     create_folder_if_not_exists(destination_path)
     destination_path = f'{destination_path}/'
-
     ## Save parameters, input settings and experiment description in script string
     experiment_setup_info = {'Experiment_Name': experiment_name}
     experiment_setup_info['Description'] = experiment_description
@@ -84,7 +53,104 @@ def main(experiment_name, experiment_description, train_file_name, eval_file_nam
     experiment_setup_info['Sample_Data_Start'] = data_cut_front
     experiment_setup_info['Sample_Data_End'] = data_cut_back
     experiment_setup_info['Save_Best_Models'] = save_model
+    for i in range(1, 5):
+        # Load binary model script with parameters
+        # Run Training and additionally save best val model as well as its epoch of origin
+        statistics, e_loss, eval_acc, eval_kappa, best_val_epoch = run_binary_classification(
+            batch_size, shuffle, workers, max_epochs,
+            f'{base_save_path}_class{i}_train_data.npy', f'{base_save_path}_class{i}_train_labels.npy',
+            f'{base_save_path}_class{i}_validate_data.npy', f'{base_save_path}_class{i}_validate_labels.npy',
+            f'{base_save_path}cwt_eval_class{i}.npy', f'{base_save_path}normalized_eval_class{i}_labels.npy',
+            model_channels, model_classes, model_dropout,
+            model_learning_rate, model_weight_decay, data_cut_front, data_cut_back, save_model,
+            f'{base_save_path}{experiment_name}_class{i}_model', device
+        )
+        experiment_setup_info[f'Class_{i}_Evaluation_Loss'] = e_loss.item()
+        experiment_setup_info[f'Class_{i}_Evaluation_Accuracy'] = eval_acc
+        experiment_setup_info[f'Class_{i}_Evaluation_Kappa'] = eval_kappa.item()
+        experiment_setup_info[f'Class_{i}_Best_Validation_Epoch'] = best_val_epoch
 
+        # Save Training Run Statistics
+        train_statistics = {
+            'epoch': statistics[0]
+            , 'train_loss': statistics[1]
+            , 'train_acc': statistics[2]
+            , 'val_loss': statistics[3]
+            , 'val_acc': statistics[4]
+        }
+        ## save statistic of training
+        with open(f'{destination_path}train_statistics_class{i}.json', 'w') as stats_file:
+            json.dump(train_statistics, stats_file)
+
+        # If feature extraction is True move filters file
+        if extract_features:
+            shutil.copyfile(f'{base_save_path}normalized_train_filters.npy',
+                            f'{destination_path}normalized_train_filters.npy')
+
+        # load saved best val model and apply eval set
+        if save_model:
+            e_loss, eval_acc, eval_kappa = load_and_run_eval(
+                f'{base_save_path}{experiment_name}_class{i}_model.pth'
+                , f'{base_save_path}cwt_eval_class{i}.npy', f'{base_save_path}normalized_eval_class{i}_labels.npy'
+                , data_cut_front, data_cut_back, model_channels, model_classes, model_dropout, device)
+            ## save statistic of eval on best val model
+            experiment_setup_info[f'Best_Model_Class_{i}_Evaluation_Loss'] = e_loss.item()
+            experiment_setup_info[f'Best_Model_Class_{i}_Evaluation_Accuracy'] = eval_acc
+            experiment_setup_info[f'Best_Model_Class_{i}_Evaluation_Kappa'] = eval_kappa.item()
+    # call the multiclass function on all 4 models
+    best_acc, best_kappa, last_acc, last_kappa = multiclass_run(base_save_path, experiment_name, 4
+                                                                , data_cut_front, data_cut_back, model_channels,
+                                                                model_classes, model_dropout, 'cpu')
+    experiment_setup_info[f'MultiClass_Best_Evaluation_Accuracy'] = best_acc
+    experiment_setup_info[f'MultiClass_Best_Evaluation_Kappa'] = best_kappa
+    experiment_setup_info[f'MultiClass_Last_Evaluation_Accuracy'] = last_acc
+    experiment_setup_info[f'MultiClass_Last_Evaluation_Kappa'] = last_kappa
+    print(f'Multiclass Accuarcy best: {best_acc}, last: {last_acc}')
+    # Save The experiment setup String in Experiments folder
+    with open(f'{destination_path}experiment_description.json', 'w') as exp_file:
+        json.dump(experiment_setup_info, exp_file)
+    result_collector[experiment_number]['best_acc'] = best_acc
+    result_collector[experiment_number]['last_acc'] = last_acc
+    result_collector[experiment_number]['c_params'] = experiment_setup_info
+    return best_acc, experiment_setup_info, last_acc
+
+
+def main(experiment_name, experiment_description, train_file_name, eval_file_name, eval_label_file_name,
+         learning_rates, weight_decays, cut_offs, dropouts,
+         result_collector={}, experiment_number=0, device='cuda', max_gpus=1, process_per_gpu=1):
+    file_directory = os.path.dirname(os.path.abspath(__file__))
+    parent_folder = os.path.dirname(file_directory)
+    base_save_path = os.path.join(file_directory, 'temp/')
+
+    # Create temp folder if not exists
+    create_temp_folder(file_directory)
+
+    experiment_description = experiment_description
+    train_file_name = train_file_name
+    eval_file_name = eval_file_name
+    eval_label_file_name = eval_label_file_name
+
+    base_file_path = os.path.join(parent_folder, 'original_data/Datasets/BCICompetitionIV/Data/BCICIV_2a_gdf/')
+    base_label_path = os.path.join(parent_folder, 'savecopywithlabels/')
+
+    low_pass = 7
+    high_pass = 30
+    raw_train_file_name = os.path.join(base_file_path, train_file_name)
+    raw_eval_file_name = os.path.join(base_file_path, eval_file_name)
+    extract_features = False
+    scale_1 = [7,15,0.5]
+    scale_2 = [16,30,0.5]
+    split_ratio = 0.7
+    splitting_strategy = 'balanced-copy'
+    batch_size = 32
+    shuffle = True
+    workers = 10
+    max_epochs = 100
+    model_channels = 8
+    model_classes = 2
+
+
+    ## DATA PREP START
     # load raw file into temp folder
     load_eeg_from_gdf(low_pass, high_pass, raw_train_file_name, f'{base_save_path}raw_train', frequency=250, trial_duration=6)
     load_eeg_from_gdf(low_pass, high_pass, raw_eval_file_name, f'{base_save_path}raw_eval', frequency=250,
@@ -117,67 +183,70 @@ def main(experiment_name, experiment_description, train_file_name, eval_file_nam
         # Prepare Train and Val sets
         data_splitter = DataSplitter(f'{base_save_path}cwt_train_class{i}.npy', f'{base_save_path}normalized_train_class{i}_labels.npy', f'{base_save_path}_class{i}', split_ratio)
         data_splitter.split(splitting_strategy)
+    ## DATA PREP END
 
-        # Load binary model script with parameters
-        # Run Training and additionally save best val model as well as its epoch of origin
-        statistics, e_loss, eval_acc, eval_kappa, best_val_epoch = run_binary_classification(
-            batch_size, shuffle, workers, max_epochs,
-            f'{base_save_path}_class{i}_train_data.npy', f'{base_save_path}_class{i}_train_labels.npy',
-            f'{base_save_path}_class{i}_validate_data.npy', f'{base_save_path}_class{i}_validate_labels.npy',
-            f'{base_save_path}cwt_eval_class{i}.npy', f'{base_save_path}normalized_eval_class{i}_labels.npy',
-            model_channels, model_classes, model_dropout,
-            model_learning_rate, model_weight_decay, data_cut_front, data_cut_back, save_model, f'{base_save_path}{experiment_name}_class{i}_model', device
-        )
-        experiment_setup_info[f'Class_{i}_Evaluation_Loss'] = e_loss.item()
-        experiment_setup_info[f'Class_{i}_Evaluation_Accuracy'] = eval_acc
-        experiment_setup_info[f'Class_{i}_Evaluation_Kappa'] = eval_kappa.item()
-        experiment_setup_info[f'Class_{i}_Best_Validation_Epoch'] = best_val_epoch
+    ## Threaded Model Start
+    subject_experiments_amount = len(learning_rates) * len(weight_decays) * len(cut_offs) * len(dropouts)
+    start_expriment_nr = int(experiment_number)
+    current_experiment_nr = start_expriment_nr
 
-        # Save Training Run Statistics
-        train_statistics = {
-            'epoch' : statistics[0]
-            ,'train_loss': statistics[1]
-            ,'train_acc': statistics[2]
-            ,'val_loss': statistics[3]
-            ,'val_acc': statistics[4]
-        }
-        ## save statistic of training
-        with open(f'{destination_path}train_statistics_class{i}.json', 'w') as stats_file:
-            json.dump(train_statistics, stats_file)
+    max_gpus = int(max_gpus)
+    process_per_gpu=int(process_per_gpu)
 
-        # If feature extraction is True move filters file
-        if extract_features:
-            shutil.copyfile(f'{base_save_path}normalized_train_filters.npy', f'{destination_path}normalized_train_filters.npy')
+    max_threads = max_gpus * process_per_gpu
+    current_threads = []
+    overall_best_accuracy = -math.inf
+    best_params = {}
 
-        # load saved best val model and apply eval set
-        if save_model:
-            e_loss, eval_acc, eval_kappa = load_and_run_eval(
-                f'{base_save_path}{experiment_name}_class{i}_model.pth'
-                , f'{base_save_path}cwt_eval_class{i}.npy', f'{base_save_path}normalized_eval_class{i}_labels.npy'
-                ,data_cut_front, data_cut_back, model_channels, model_classes, model_dropout, device)
-            ## save statistic of eval on best val model
-            experiment_setup_info[f'Best_Model_Class_{i}_Evaluation_Loss'] = e_loss.item()
-            experiment_setup_info[f'Best_Model_Class_{i}_Evaluation_Accuracy'] = eval_acc
-            experiment_setup_info[f'Best_Model_Class_{i}_Evaluation_Kappa'] = eval_kappa.item()
+    while current_experiment_nr < subject_experiments_amount:
+        if len(current_threads) < max_threads:
+            experiment_setup_info = {}
+            learning_rate = learning_rates[
+                current_experiment_nr // (len(cut_offs) * len(dropouts) * len(weight_decays)) % len(
+                    learning_rates)]  # fourth
+            weight_decay = weight_decays[
+                current_experiment_nr // (len(cut_offs) * len(dropouts)) % len(weight_decays)]  # third
+            dropout = dropouts[current_experiment_nr // (len(cut_offs)) % len(dropouts)]  # second
+            cut_off = cut_offs[current_experiment_nr % len(cut_offs)]  # first
+            cut_off_front = cut_off[0]
+            cut_off_back = cut_off[1]
 
-    # call the multiclass function on all 4 models
-    best_acc, best_kappa, last_acc, last_kappa = multiclass_run(base_save_path, experiment_name, 4
-         ,data_cut_front, data_cut_back, model_channels, model_classes, model_dropout, 'cpu')
-    experiment_setup_info[f'MultiClass_Best_Evaluation_Accuracy'] = best_acc
-    experiment_setup_info[f'MultiClass_Best_Evaluation_Kappa'] = best_kappa
-    experiment_setup_info[f'MultiClass_Last_Evaluation_Accuracy'] = last_acc
-    experiment_setup_info[f'MultiClass_Last_Evaluation_Kappa'] = last_kappa
+            device = f'cuda:{len(current_threads) // process_per_gpu}'
 
-    print(f'Multiclass Accuarcy best: {best_acc}, last: {last_acc}')
+            current_threads.append(threading.Thread(target=run_threaded_model, args=(base_save_path, batch_size, cut_off_back,
+                                                                           cut_off_front, device, dropout,
+                                                                           eval_file_name, experiment_description,
+                                                                           experiment_name, experiment_number,
+                                                                           experiment_setup_info, extract_features,
+                                                                           file_directory, high_pass, learning_rate,
+                                                                           low_pass, max_epochs, model_channels,
+                                                                           model_classes, result_collector, scale_1,
+                                                                           scale_2, shuffle, split_ratio,
+                                                                           splitting_strategy, train_file_name,
+                                                                           weight_decay, workers)))
+            current_experiment_nr += 1
+        else:
+            for prepared_thread in current_threads:
+                prepared_thread.start()
+            for prepared_thread in current_threads:
+                prepared_thread.join()
+            current_threads = []
 
-    # Save The experiment setup String in Experiments folder
-    with open(f'{destination_path}experiment_description.json', 'w') as exp_file:
-        json.dump(experiment_setup_info, exp_file)
+    for i, exp_key in enumerate(list(result_collector.keys())):
+        exp_dic = result_collector[exp_key]
+        if exp_dic['best_acc'] > overall_best_accuracy or exp_dic['last_acc'] > overall_best_accuracy:
+            overall_best_accuracy = max(exp_dic['best_acc'], exp_dic['last_acc'])
+            params = exp_dic['c_params']
+            params['best_accuracy_multi'] = overall_best_accuracy
+            print(f'New best Config Found for subject file {train_file_name}')
+            print(params)
+            best_params = params
+    ## Threaded Experiments End
+
     # Delete the temp Folder
     delete_temp_folder(file_directory)
-    result_collector[experiment_number]['best_acc'] = best_acc
-    result_collector[experiment_number]['last_acc'] = last_acc
-    return best_acc, last_acc
+    return best_params
+
 
 if __name__ == "__main__":
 	main(*sys.argv[1:])
--- Scripts/run_gdf_clean_experiments.py
+++ Scripts/run_gdf_clean_experiments.py
@@ -9,87 +9,38 @@ weight_decays = [0.5,0.1,0.05,0.01,0.005,0.001,0.0005,0.0001]
 cut_offs = [[0,200],[25,225],[50,250],[75,275],[100,300]]
 dropouts = [0.1,0.3,0.5,0.7]
 nr = ['01', '02', '03', '05', '06', '07', '08', '09']
-subject_experiments_amount = len(learning_rates) * len(weight_decays) * len(cut_offs) * len(dropouts)
-total_experiments_amount = subject_experiments_amount * len(nr)
 
 
-def main(start_expriment_nr=0,max_gpus=1, process_per_gpu=1, subject_experiments_amount=subject_experiments_amount, total_experiments_amount=total_experiments_amount):
+def main(start_expriment_nr=797,max_gpus=1, process_per_gpu=1):
     start_expriment_nr = int(start_expriment_nr)
     max_gpus = int(max_gpus)
     process_per_gpu=int(process_per_gpu)
-
-
-    current_experiment_nr = start_expriment_nr
-    max_threads = max_gpus * process_per_gpu
-    current_threads = []
     best_params = [None for x in range(len(nr))]
-    overall_best_accuracy = [-math.inf for i in range(len(nr))]
-
-    result_collector = {}
-
-    while current_experiment_nr < total_experiments_amount:
-        if len(current_threads) < max_threads:
-            n = nr[current_experiment_nr//subject_experiments_amount]
-            learning_rate = learning_rates[
-                current_experiment_nr // (len(cut_offs) * len(dropouts) * len(weight_decays)) % len(
-                    learning_rates)]  # fourth
-            weight_decay = weight_decays[
-                current_experiment_nr // (len(cut_offs) * len(dropouts)) % len(weight_decays)]  # third
-            dropout = dropouts[current_experiment_nr // (len(cut_offs)) % len(dropouts)]  # second
-            cut_off = cut_offs[current_experiment_nr % len(cut_offs)]  # first
-            cut_off_front = cut_off[0]
-            cut_off_back = cut_off[1]
-            experiment_name = f'Binary_ANN_A{n}_learnrate{learning_rate}_weightdec{weight_decay}_cutoff{cut_off_front}_{cut_off_back}_drop{dropout}'
-            experiment_description = 'One vs Rest classification of GDF based motor imagery classifcation on ANN Architecture. ' \
-                                     'Applies Error averaging, CSP, Normalization and then transforms into CWT'
-            train_file_name = f'A{n}T.gdf'
-            eval_file_name = f'A{n}E.gdf'
-            eval_label_file_name = f'A{n}E_labels.npy'
-            c_params = {'learning_rate': learning_rate
-                , 'weight_decay': weight_decay
-                , 'cut_off_front': cut_off_front
-                , 'cut_off_back': cut_off_back
-                , 'dropout': dropout
-                        #        , 'best_accuracy_multi': max(best_acc, last_acc)
-                        }
-            # result_collector key is experiment number, added is a dic with best_acc, last_acc, c_params
-            device = f'cuda:{len(current_threads) // process_per_gpu}'
-
-            result_collector[current_experiment_nr] = {'c_params': c_params}
-            current_threads.append(threading.Thread(target=run_training,
-                                                    args=(experiment_name, experiment_description,
-                                                          train_file_name, eval_file_name, eval_label_file_name,
-                                                          learning_rate, weight_decay, cut_off_front, cut_off_back,
-                                                          dropout,
-                                                          result_collector, current_experiment_nr, device)))
-            current_experiment_nr += 1
-        else:
-            for prepared_thread in current_threads:
-                prepared_thread.start()
-            for prepared_thread in current_threads:
-                prepared_thread.join()
-            current_threads = []
 
 
-    for i, exp_key in enumerate(list(result_collector.keys())):
-        exp_dic = result_collector[exp_key]
-        if exp_dic['best_acc'] > overall_best_accuracy[i // subject_experiments_amount] or exp_dic['last_acc'] > \
-                overall_best_accuracy[i // subject_experiments_amount]:
-            overall_best_accuracy[i // subject_experiments_amount] = max(exp_dic['best_acc'], exp_dic['last_acc'])
-            params = exp_dic['c_params']
-            params['best_accuracy_multi'] = overall_best_accuracy[i // subject_experiments_amount]
-            print(f'New best Config Found for subject {i // subject_experiments_amount}')
-            print(params)
-            best_params[i // subject_experiments_amount] = [f'ANN_{i // subject_experiments_amount}', params]
+    for i, n in enumerate(nr):
+        result_collector = {}
+        experiment_name = f'Binary_ANN_A{n}'
+        experiment_description = 'One vs Rest classification of GDF based motor imagery classifcation on ANN Architecture. ' \
+                                 'Applies Error averaging, CSP, Normalization and then transforms into CWT'
+        train_file_name = f'A{n}T.gdf'
+        eval_file_name = f'A{n}E.gdf'
+        eval_label_file_name = f'A{n}E_labels.npy'
+        device = f'cuda'
+        best_params_s = run_training(experiment_name, experiment_description,
+                                     train_file_name, eval_file_name, eval_label_file_name,
+                                     learning_rates, weight_decays, cut_offs,
+                                     dropouts,
+                                     result_collector, start_expriment_nr, device, max_gpus, process_per_gpu)
 
+        best_params[i] = best_params_s
 
-    with open('ANN_GDFs_HyperResults.txt','w+') as final_file:
-        for r in best_params:
-            params = r[1]
-            final_file.write(f'Subject: {r[0]}, learning_rate: {params["learning_rate"]}, '
-                             f'weight_decay:{params["weight_decay"]}, cut_off_front: {params["cut_off_front"]}, '
-                             f'cut_off_back: {params["cut_off_back"]}, dropout: {params["dropout"]}, '
-                             f'ACC: {params["best_accuracy_multi"]}/n/n')
+    with open('ANN_GDF_HyperResults.txt', 'w+') as final_file:
+        for i, r in enumerate(best_params):
+            final_file.write(f'Subject: {i}, learning_rate: {r["Learning_Rate"]}, '
+                             f'weight_decay:{r["Weight_Decay"]}, cut_off_front: {r["Sample_Data_Start"]}, '
+                             f'cut_off_back: {r["Sample_Data_End"]}, dropout: {r["Dropout_Value"]}, '
+                             f'ACC: {r["MultiClass_Best_Evaluation_Accuracy"]}/n/n')
 
 
 if __name__ == '__main__':
