ca8c5f2e3a0a0cd95b3a79780d883972ed43908b
skiwan <merlinsewia@web.de>
2022-01-13 14:32:47 +0100

    Implements SNN LIF Architecture and prepapres binarz snn classifier

--- Models/SNN/BinaryEEGClassifierLIF.py
+++ Models/SNN/BinaryEEGClassifierLIF.py
@@ -1,15 +1,15 @@
-from norse.torch.module import Izhikevich
-from norse.torch import SequentialState, LIF
+from norse.torch.module import LIFCell
+from norse.torch import SequentialState
 import torch
 
 class BinaryEEGClassifierLIF(torch.nn.Module):
-  def __init__(self, p):
+  def __init__(self, input_channels):
     super().__init__()
     self.model = SequentialState(
-    torch.nn.Linear(100,200),
-    Izhikevich(spiking_method=p),
-    torch.nn.Linear(200,100),
-    Izhikevich(spiking_method=p)
+    torch.nn.Linear(input_channels,2),
+    LIFCell(2,2),
+    torch.nn.Linear(2,2),
+    LIFCell(2,1)
     )
 
   def forward(self, x):
---
+++ Scripts/binary_class_snn_run.py
@@ -0,0 +1,16 @@
+from Models.ANN.BaseSCNN import BaseSCNN
+from Models.CustomDataset import CustomDataset
+from torch.utils.data import DataLoader
+import torch
+import torch.optim as optim
+import numpy as np
+
+import matplotlib.pyplot as plt
+import logging
+
+
+def load_and_run_eval():
+    pass
+
+def run_binary_classification():
+       pass
