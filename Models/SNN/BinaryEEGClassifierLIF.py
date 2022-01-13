2da306efcde642cef5589764ccba768e67abf8f9
skiwan <merlinsewia@web.de>
2022-01-13 15:03:18 +0100

    Prepares psuedoCode for binary snn classiffier

--- Models/SNN/BinaryEEGClassifierLIF.py
+++ Models/SNN/BinaryEEGClassifierLIF.py
@@ -1,5 +1,5 @@
 from norse.torch.module import LIFCell
-from norse.torch import SequentialState
+from norse.torch import SequentialState, LIF
 import torch
 
 class BinaryEEGClassifierLIF(torch.nn.Module):
@@ -7,7 +7,7 @@ class BinaryEEGClassifierLIF(torch.nn.Module):
     super().__init__()
     self.model = SequentialState(
     torch.nn.Linear(input_channels,2),
-    LIFCell(2,2),
+    LIFCell(2,2), # use lif here?
     torch.nn.Linear(2,2),
     LIFCell(2,1)
     )
--- Scripts/binary_class_snn_run.py
+++ Scripts/binary_class_snn_run.py
@@ -11,6 +11,59 @@ import logging
 
 def load_and_run_eval():
     pass
+    # Create Model and Load Model
+    # load training set
+    # Generate average spike frequencies
+    # set model to eval mode
+    # load eval files
+    # for eval data
+        # generate output
+        # transform to labels via average spike frequency
+        # generate stastics
+    # return statistics
+
 
 def run_binary_classification():
-       pass
+    pass
+    # set parameters
+    # get train val and eval files
+    # create custom datasets
+    # prepare data loaders
+    # create model, optimizer and loss function, prepare best epoch and min valid loss as well as statistics, prepare spike average array
+
+    # generate average spike frequncy rates for class and non class
+        # load all samples, sum all 1 and 0 spike trains and generate averages, save as comparison value
+
+    # Beginn of Training for each epoch
+
+        # loss and acc
+
+        # for each batch
+            # transform data
+            # generate outputs
+            # convert outputs to closest class based on average spike frequncy
+            # compute loss
+            # backward loss
+            # optimzer step
+        # train_loss = train acc
+
+        # generate average spike frequncy rates for class and non class
+            # load all samples, sum all 1 and 0 spike trains and generate averages, save as comparison value
+
+        # validation phase
+            # same as training steps but without optimizer step
+        # val loss and acc
+
+        # log info
+        # append statistics
+        # check if val loss is reduced
+            # save model
+
+    # Beginn of Eval
+        # Load Eval set
+        # Same as above
+        # generate eval acc and print\log
+    # save last model
+    #combine and return statistics
+    #
+
