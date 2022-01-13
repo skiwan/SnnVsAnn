e8d513e28f80f31d2b6c9bd08eee69d9981152dc
skiwan <merlinsewia@web.de>
2022-01-13 16:22:03 +0100

    First implementation of binary snn training

--- Scripts/binary_class_snn_run.py
+++ Scripts/binary_class_snn_run.py
@@ -61,7 +61,7 @@ def run_binary_classification(
     # create model, optimizer and loss function, prepare best epoch and min valid loss as well as statistics, prepare spike average array
     model = BinaryEEGClassifierLIF(channels=model_channels).to(device)
     optimizer = optim.Adam(model.parameters(), lr=model_learning_rate, weight_decay=model_weight_decay)
-    criterion = torch.nn.CrossEntropyLoss()
+    criterion = torch.nn.L1Loss()
 
     min_valid_loss = np.inf
     best_val_epoch = -1
@@ -78,45 +78,86 @@ def run_binary_classification(
 
     # load all samples, sum all 1 and 0 spike trains and generate averages, save as comparison value
     for data, labels in training_generator:
-        pass
         data = data[:, :, :, data_cut_front:data_cut_back].float()
         data = data.to(device)
         labels = labels.long()
         labels = labels.to(device)
         outputs = model(data)
-        outputs = outputs.sum() # add dim?
+        outputs = outputs.sum(dim=1)
         for i, x in enumerate(labels):
             c_label = int(x)
             spikes = outputs[i]
             spike_frequencies[c_label] =+ spikes
             sample_amount[c_label] =+ 1
 
-    spike_frequencies = (np.array(spike_frequencies)/np.array(sample_amount)).tolist()
+    spike_frequencies = (np.array(spike_frequencies)/np.array(sample_amount))
 
     # Beginn of Training for each epoch
-
+    for epoch in range(max_epochs):
         # loss and acc
-
+        train_loss = 0.0
+        train_mae_acc = 0.0
         # for each batch
+        for data, labels in training_generator:
             # transform data
+            data = data[:, :, :, data_cut_front:data_cut_back].float()
+            data = data.to(device)
+            labels = labels.long()
+            # Convert class labels to target spike frequencies
+            s_labels = [spike_frequencies[i] for i in labels]
+            s_labels = labels.to(device)
             # generate outputs
-            # convert outputs to closest class based on average spike frequncy
+            optimizer.zero_grad()
+            outputs = model(data)
+            # convert spike trains to sum of spikes
+            outputs.sum(dim=1)
             # compute loss
+            loss = criterion(outputs, s_labels)
             # backward loss
+            loss.backward()
             # optimzer step
-        # train_loss = train acc
+            optimizer.step()
+            train_loss += loss.item()
 
-        # generate average spike frequncy rates for class and non class
-            # load all samples, sum all 1 and 0 spike trains and generate averages, save as comparison value
+            # convert spike trains to closest label for acc prediction
+            distances = np.array([x - spike_frequencies for x in outputs])
+            distances = np.absolute(distances)
+            out_labels = np.argmin(distances, axis=1)
+            diff_l = [0 if out_labels[i] == labels[i] else 1 for i in range(len(labels))]
+            train_mae_acc += sum(diff_l)
+
+        # train_loss = train acc
+        l = len(training_generator)*params['batch_size']
+        train_mae_acc = 1 - (train_mae_acc/l)
+
+        # reset spike frequencies after training
+        spike_frequencies = [0 for x in model_classes]
+        sample_amount = [0 for x in model_classes]
+        for data, labels in training_generator:
+            data = data[:, :, :, data_cut_front:data_cut_back].float()
+            data = data.to(device)
+            labels = labels.long()
+            labels = labels.to(device)
+            outputs = model(data)
+            outputs = outputs.sum(dim=1)
+            for i, x in enumerate(labels):
+                c_label = int(x)
+                spikes = outputs[i]
+                spike_frequencies[c_label] = + spikes
+                sample_amount[c_label] = + 1
+        spike_frequencies = (np.array(spike_frequencies) / np.array(sample_amount))
 
         # validation phase
-            # same as training steps but without optimizer step
+        val_loss = 0.0
+        val_mae_acc = 0.0
+        # same as training steps but without optimizer step
+
         # val loss and acc
 
         # log info
         # append statistics
         # check if val loss is reduced
-            # save model
+        # save model
 
     # Beginn of Eval
         # Load Eval set
