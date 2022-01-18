029204941edd84d486fa044e506facf25af290cd
skiwan <merlinsewina@web.de>
2022-01-18 14:56:54 +0100

    Makes sure initial layer initialization for linear layers is always positive

--- Models/SNN/BinaryEEGClassifierLIF.py
+++ Models/SNN/BinaryEEGClassifierLIF.py
@@ -5,10 +5,15 @@ import torch
 class BinaryEEGClassifierLIF(torch.nn.Module):
   def __init__(self, channels=8):
     super().__init__()
+    linear_1 = torch.nn.Linear(channels,2)
+    linear_1.weight = torch.nn.Parameter(torch.abs(linear_1.weight))
+    linear_2 = torch.nn.Linear(2,1)
+    linear_2.weight = torch.nn.Parameter(torch.abs(linear_2.weight)
+
     self.model = SequentialState(
-    torch.nn.Linear(channels,2),
+    linear_1,
     LIF(),
-    torch.nn.Linear(2,1),
+    linear_2,
     LIF()
     )
 
