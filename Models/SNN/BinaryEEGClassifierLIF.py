795b017c03ee34bdf21abcd9eb17615172d9a634
skiwan <merlinsewina@web.de>
2022-01-18 15:47:14 +0100

    Dont force postivie weights

--- Models/SNN/BinaryEEGClassifierLIF.py
+++ Models/SNN/BinaryEEGClassifierLIF.py
@@ -6,9 +6,9 @@ class BinaryEEGClassifierLIF(torch.nn.Module):
   def __init__(self, channels=8):
     super().__init__()
     self.linear_1 = torch.nn.Linear(channels,2)
-    self.linear_1.weight = torch.nn.Parameter(torch.abs(self.linear_1.weight))
+    #self.linear_1.weight = torch.nn.Parameter(torch.abs(self.linear_1.weight))
     self.linear_2 = torch.nn.Linear(2,1)
-    self.linear_2.weight = torch.nn.Parameter(torch.abs(self.linear_2.weight))
+    #self.linear_2.weight = torch.nn.Parameter(torch.abs(self.linear_2.weight))
 
     self.model = SequentialState(
     self.linear_1,
