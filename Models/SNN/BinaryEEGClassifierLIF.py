48ba8000e51b1bedfbe7e103441bc8a60a80f458
skiwan <merlinsewina@web.de>
2022-01-18 14:57:27 +0100

    Fixes extremly complicated error that I never ever make

--- Models/SNN/BinaryEEGClassifierLIF.py
+++ Models/SNN/BinaryEEGClassifierLIF.py
@@ -8,7 +8,7 @@ class BinaryEEGClassifierLIF(torch.nn.Module):
     linear_1 = torch.nn.Linear(channels,2)
     linear_1.weight = torch.nn.Parameter(torch.abs(linear_1.weight))
     linear_2 = torch.nn.Linear(2,1)
-    linear_2.weight = torch.nn.Parameter(torch.abs(linear_2.weight)
+    linear_2.weight = torch.nn.Parameter(torch.abs(linear_2.weight))
 
     self.model = SequentialState(
     linear_1,
