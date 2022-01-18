2c1c70909e6ab6557eed03448f6f0f559419a365
skiwan <merlinsewina@web.de>
2022-01-18 15:53:47 +0100

    Introduceses more SNN params

--- Models/SNN/BinaryEEGClassifierLIF.py
+++ Models/SNN/BinaryEEGClassifierLIF.py
@@ -5,9 +5,9 @@ import torch
 class BinaryEEGClassifierLIF(torch.nn.Module):
   def __init__(self, channels=8):
     super().__init__()
-    self.linear_1 = torch.nn.Linear(channels,1)
+    self.linear_1 = torch.nn.Linear(channels,100)
     #self.linear_1.weight = torch.nn.Parameter(torch.abs(self.linear_1.weight))
-    self.linear_2 = torch.nn.Linear(2,1)
+    self.linear_2 = torch.nn.Linear(100,1)
     #self.linear_2.weight = torch.nn.Parameter(torch.abs(self.linear_2.weight))
 
     self.model = SequentialState(
