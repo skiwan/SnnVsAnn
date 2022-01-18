570308d0f0845613897b5e79238c76b2ea9cac2e
skiwan <merlinsewina@web.de>
2022-01-18 15:52:07 +0100

    Change SNN model slightly

--- Models/SNN/BinaryEEGClassifierLIF.py
+++ Models/SNN/BinaryEEGClassifierLIF.py
@@ -5,15 +5,15 @@ import torch
 class BinaryEEGClassifierLIF(torch.nn.Module):
   def __init__(self, channels=8):
     super().__init__()
-    self.linear_1 = torch.nn.Linear(channels,2)
+    self.linear_1 = torch.nn.Linear(channels,1)
     #self.linear_1.weight = torch.nn.Parameter(torch.abs(self.linear_1.weight))
     self.linear_2 = torch.nn.Linear(2,1)
     #self.linear_2.weight = torch.nn.Parameter(torch.abs(self.linear_2.weight))
 
     self.model = SequentialState(
     self.linear_1,
-    LIF(),
-    self.linear_2,
+    #LIF(),
+    #self.linear_2,
     LIF()
     )
 
