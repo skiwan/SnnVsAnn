1a2c511a245a0d279752f06a624f044fcde3efd5
skiwan <merlinsewina@web.de>
2022-02-21 23:32:39 +0100

    Actually append spike trains

--- Scripts/multi_class_snn_run.py
+++ Scripts/multi_class_snn_run.py
@@ -108,8 +108,8 @@ def main_return_data(base_path, base_model_name, class_amount
         last_models.append(last_model)
         eval_sets.append(torch.from_numpy(np.load(f'{base_path}normalized_eval_class{i+1}.npy')).to(device))
 
-    best_predicts = []
-    last_predicts = []
+    best_train = []
+    last_train = []
     best_models_convidence = []
     last_models_convidence = []
 
@@ -129,10 +129,12 @@ def main_return_data(base_path, base_model_name, class_amount
         data = torch.swapaxes(data, 0, 2)
         data = torch.swapaxes(data, 1, 2)
         outputs = b_model(data)
+        best_train.append(outputs)
         outputs = outputs[0].sum(dim=0)  # batch size, spikes
         outputs = torch.squeeze(outputs) # spikes per sample
         best_models_convidence.append(outputs)
         outputs = l_model(data)
+        last_train.append(outputs)
         outputs = outputs[0].sum(dim=0)  # batch size, spikes
         outputs = torch.squeeze(outputs)  # spikes per sample
         last_models_convidence.append(outputs)
