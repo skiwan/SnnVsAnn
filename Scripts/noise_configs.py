01006fbd8110446a079b935367e008942326b338
skiwan <merlin.sewina@web.de>
2022-08-29 23:57:50 +0200

    Run Gaussian noise gdf

--- Scripts/noise_configs.py
+++ Scripts/noise_configs.py
@@ -14,3 +14,18 @@ white_noise_values = [
     {"noise_strength_percent":0.9},
     {"noise_strength_percent":1}
 ]
+
+gaussian_noise_values = [
+    {"noise_strength_percent": 0.1},
+    {"noise_strength_percent":0.2},
+    {"noise_strength_percent":0.3},
+    {"noise_strength_percent":0.4},
+    {"noise_strength_percent":0.45},
+    {"noise_strength_percent":0.5},
+    {"noise_strength_percent":0.55},
+    {"noise_strength_percent":0.6},
+    {"noise_strength_percent":0.7},
+    {"noise_strength_percent":0.8},
+    {"noise_strength_percent":0.9},
+    {"noise_strength_percent":1}
+]
