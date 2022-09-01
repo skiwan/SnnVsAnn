1382195d03fdb3c7cf571b96323b8e1dac53b02a
skiwan <merlin.sewina@web.de>
2022-09-02 00:46:35 +0200

    Fixes Gaussian Noise generation

--- Scripts/noise_configs.py
+++ Scripts/noise_configs.py
@@ -1,18 +1,25 @@
 
 
 white_noise_values = [
-    {"noise_strength_percent": 0.1},
-    {"noise_strength_percent":0.2},
-    {"noise_strength_percent":0.3},
-    {"noise_strength_percent":0.4},
-    {"noise_strength_percent":0.45},
-    {"noise_strength_percent":0.5},
-    {"noise_strength_percent":0.55},
-    {"noise_strength_percent":0.6},
-    {"noise_strength_percent":0.7},
-    {"noise_strength_percent":0.8},
-    {"noise_strength_percent":0.9},
-    {"noise_strength_percent":1}
+    #{"noise_strength_percent": 0.1},
+    #{"noise_strength_percent":0.2},
+    #{"noise_strength_percent":0.3},
+    #{"noise_strength_percent":0.4},
+    #{"noise_strength_percent":0.45},
+    #{"noise_strength_percent":0.5},
+    #{"noise_strength_percent":0.55},
+    #{"noise_strength_percent":0.6},
+    #{"noise_strength_percent":0.7},
+    #{"noise_strength_percent":0.8},
+    #{"noise_strength_percent":0.9},
+    #{"noise_strength_percent":1},
+    {"noise_strength_percent": 1.2},
+    {"noise_strength_percent": 1.4},
+    {"noise_strength_percent": 1.6},
+    {"noise_strength_percent": 1.8},
+    {"noise_strength_percent": 2},
+    {"noise_strength_percent": 3},
+    {"noise_strength_percent": 5},
 ]
 
 gaussian_noise_values = [
@@ -27,5 +34,12 @@ gaussian_noise_values = [
     {"noise_strength_percent":0.7},
     {"noise_strength_percent":0.8},
     {"noise_strength_percent":0.9},
-    {"noise_strength_percent":1}
+    {"noise_strength_percent":1.0},
+    {"noise_strength_percent": 1.2},
+    {"noise_strength_percent": 1.4},
+    {"noise_strength_percent": 1.6},
+    {"noise_strength_percent": 1.8},
+    {"noise_strength_percent": 2},
+    {"noise_strength_percent": 3},
+    {"noise_strength_percent": 5},
 ]
--- apply_gaussian_noise.py
+++ apply_gaussian_noise.py
@@ -3,17 +3,20 @@ import numpy as np
 
 def apply_guassian_noise(raw_data, noise_strength_percent):
 	new_data = []
-	channels = raw_data.shape[0] #?
-	ts = raw_data.shape[1]
-	for c in range(channels):
-		c_data = raw_data[c]
-		max_abs = max([abs(max(c_data)),abs(min(c_data))])
-		max_abs = max_abs * noise_strength_percent
-		noise = np.random.normal(loc=0.0, scale=max_abs/3, size=ts)
-		noise = [min(x, max_abs) for x in noise]
-		noise = [max(x, -max_abs) for x in noise]
-		n_c_data = c_data + noise
-		new_data.append(n_c_data)
+	channels = raw_data.shape[1] #?
+	ts = raw_data.shape[2]
+	for sample in raw_data:
+		new_sample = []
+		for c in range(channels):
+			c_data = sample[c]
+			max_abs = max([abs(max(c_data)),abs(min(c_data))])
+			max_abs = max_abs * noise_strength_percent
+			noise = np.random.normal(loc=0.0, scale=max_abs/3, size=ts)
+			noise = [min(x, max_abs) for x in noise]
+			noise = [max(x, -max_abs) for x in noise]
+			n_c_data = c_data + noise
+			new_sample.append(n_c_data)
+		new_data.append(new_sample)
 	new_data = np.array(new_data)
 	return new_data
 
