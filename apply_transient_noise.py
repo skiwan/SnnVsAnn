5e5a5dfe67290218fc20457f12b69325be76b9bf
skiwan <merlin.sewina@web.de>
2022-09-02 00:49:49 +0200

    Fixes transient and burst noise

--- apply_transient_noise.py
+++ apply_transient_noise.py
@@ -3,22 +3,25 @@ import numpy as np
 
 def apply_transient_noise(raw_data, noise_strength_percent, burst_amounts, burst_length):
 	new_data = []
-	channels = raw_data.shape[0] #?
-	ts = raw_data.shape[1]
-	for c in range(channels):
-		c_data = raw_data[c]
-		max_abs = max([abs(max(c_data)),abs(min(c_data))])
-		max_abs = max_abs * noise_strength_percent
-		noise = np.zeroes(burst_length)
-		downfall = max_abs / burst_length
-		for i in range(burst_length):
-			noise[i] = max_abs - downfall * i
+	channels = raw_data.shape[1] #?
+	ts = raw_data.shape[2]
+	for sample in raw_data:
+		new_sample = []
+		for c in range(channels):
+			c_data = sample[c]
+			max_abs = max([abs(max(c_data)),abs(min(c_data))])
+			max_abs = max_abs * noise_strength_percent
+			noise = np.zeroes(burst_length)
+			downfall = max_abs / burst_length
+			for i in range(burst_length):
+				noise[i] = max_abs - downfall * i
 
-		noise_start_points = np.random.randint(low=0, high=ts-burst_length, size=burst_amounts)
+			noise_start_points = np.random.randint(low=0, high=ts-burst_length, size=burst_amounts)
 
-		for start_point in noise_start_points:
-			c_data[start_point:start_point+burst_length] += noise
-		new_data.append(c_data)
+			for start_point in noise_start_points:
+				c_data[start_point:start_point+burst_length] += noise
+			new_sample.append(c_data)
+		new_data.append(new_sample)
 	new_data = np.array(new_data)
 	return new_data
 
